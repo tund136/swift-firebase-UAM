888ecf78cf3644b313f68274c3f833a683f823d8
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-12 19:58:14 +0700

    Create the project

---
+++ FirebaseUAM/FirebaseUAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseUAMApp.swift
+//  FirebaseUAM
+//
+//  Created by Danh Tu on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseUAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
