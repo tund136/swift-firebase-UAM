8bce571ea5290e8696286bf62416d79d3269b8f6
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-13 08:35:22 +0700

    Building Login View

--- FirebaseUAM/Features/Login/View/LoginView.swift
+++ FirebaseUAM/Features/Login/View/LoginView.swift
@@ -9,12 +9,43 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 16) {
+            VStack(spacing: 16) {
+                InputTextFieldView(text: .constant(""), placeholder: "Email", keyboardType: .emailAddress, sfSymbol: "envelope")
+                
+                InputPasswordView(password: .constant(""), placeholder: "Password", sfSymbol: "lock")
+            }
+            
+            HStack {
+                Spacer()
+                
+                Button(action: {
+                    // TODO: Handle presentation to forgot password
+                }, label: {
+                    Text("Forgot Password")
+                })
+                    .font(.system(size: 16, weight: .bold))
+            }
+            
+            VStack(spacing: 16) {
+                ButtonView(title: "Login", handler: {
+                    // TODO: Handle login action
+                })
+                
+                ButtonView(title: "Register", background: .clear, foreground: .blue, border: .blue, handler: {
+                    // TODO: Handle register action
+                })
+            }
+        }
+        .padding(.horizontal)
+        .navigationTitle("Login")
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        NavigationView {
+            LoginView()
+        }
     }
 }
