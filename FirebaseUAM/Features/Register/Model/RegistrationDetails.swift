d53f97406bafb8b92ed0489db9690f82c914c811
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-13 10:58:54 +0700

    Create Registration Model

---
+++ FirebaseUAM/Features/Register/Model/RegistrationDetails.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationDetails.swift
+//  FirebaseUAM
+//
+//  Created by Danh Tu on 13/10/2021.
+//
+
+import Foundation
+
+struct RegistrationDetails {
+    var email: String
+    var password: String
+    var firstName: String
+    var lastName: String
+    var occupation: String
+}
