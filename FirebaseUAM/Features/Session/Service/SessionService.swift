19ba450bf50d24c1502118a69d0e66b441bdf74e
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-13 12:56:12 +0700

    Create Session Service

---
+++ FirebaseUAM/Features/Session/Service/SessionService.swift
@@ -0,0 +1,74 @@
+//
+//  SessionService.swift
+//  FirebaseUAM
+//
+//  Created by Danh Tu on 13/10/2021.
+//
+
+import Foundation
+import Combine
+import FirebaseAuth
+import FirebaseDatabase
+
+enum SessionState {
+    case loggedIn
+    case loggedOut
+}
+
+protocol SessionService {
+    var state: SessionState { get }
+    var userDetails: SessionUserDetails? { get }
+    func logout()
+}
+
+final class SessionServiceImpl: SessionService, ObservableObject {
+    @Published var state: SessionState = .loggedOut
+    @Published var userDetails: SessionUserDetails?
+    
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        setupFirebaseAuthHandler()
+    }
+    
+    func logout() {
+        
+    }
+}
+
+private extension SessionServiceImpl {
+    func setupFirebaseAuthHandler() {
+        handler = Auth.auth().addStateDidChangeListener { [weak self] res, user in
+            guard let self = self
+            else {
+                return
+            }
+            self.state = user == nil ?.loggedOut : .loggedIn
+            if let uid = user?.uid {
+                self.handleRefresh(with: uid)
+            }
+        }
+    }
+    
+    func handleRefresh(with uid: String) {
+        Database
+            .database()
+            .reference()
+            .child("users")
+            .child(uid)
+            .observe(.value) { [weak self] snapshot in
+                guard let self = self,
+                      let value = snapshot.value as? NSDictionary,
+                      let firstName = value[RegistrationKeys.firstName.rawValue] as? String,
+                      let lastName = value[RegistrationKeys.lastName.rawValue] as? String,
+                      let occupation = value[RegistrationKeys.occupation.rawValue] as? String
+                else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.userDetails = SessionUserDetails(firstName: firstName, lastName: lastName, occupation: occupation)
+                }
+            }
+    }
+}
