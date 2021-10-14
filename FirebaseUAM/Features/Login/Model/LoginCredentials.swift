df330e8a519b9427f5d132edc2982371e6329f01
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-14 08:46:21 +0700

    Create LoginCredentials file

---
+++ FirebaseUAM/Features/Login/Model/LoginCredentials.swift
@@ -0,0 +1,13 @@
+//
+//  LoginCredentials.swift
+//  FirebaseUAM
+//
+//  Created by Danh Tu on 14/10/2021.
+//
+
+import Foundation
+
+struct LoginCredentials {
+    var email: String
+    var password: String
+}
