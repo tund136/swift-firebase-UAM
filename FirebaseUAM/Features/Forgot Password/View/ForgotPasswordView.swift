7b59f6ecaa5e9e10a5027b9d53fab73cfef54a01
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-14 09:59:11 +0700

    Writing Forgot Password logic using Combine & Firebase

--- FirebaseUAM/Features/Forgot Password/View/ForgotPasswordView.swift
+++ FirebaseUAM/Features/Forgot Password/View/ForgotPasswordView.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct ForgotPasswordView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject private var vm = ForgotPasswordViewModelImpl(
+        service: ForgotPasswordServiceImpl()
+    )
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
-                InputTextFieldView(text: .constant(""), placeholder: "Email", keyboardType: .emailAddress, sfSymbol: "envelope")
+                InputTextFieldView(text: $vm.email, placeholder: "Email", keyboardType: .emailAddress, sfSymbol: "envelope")
                 
                 ButtonView(title: "Send Password Reset", handler: {
                     // TODO: Handle password reset
+                    vm.sendPasswordReset()
+                    presentationMode.wrappedValue.dismiss()
                 })
             }
             .padding(.horizontal)
